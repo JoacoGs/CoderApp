b6f2c1473e9d86c3c5fb404d96835c1444d18ef4
Joaquin Gimenez <js.gimenez98@gmail.com>
2022-11-23 21:48:23 -0300

    Desafio 4

--- App.js
+++ App.js
@@ -1,41 +1,14 @@
-import { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import Header from "./components/Header";
-import GameScreen from "./screens/GameScreen";
-import StartGameScreen from "./screens/StartGameScreen";
 import { useFonts } from "expo-font";
+import ShopNavigator from "./src/navigation/ShopNavigator";
 
 export default function App() {
-  const [loaded] = useFonts({
-    Sono: require("./assets/fonts/Sono-Regular.ttf"),
+  const [fontsLoaded] = useFonts({
+    Sono: require("./src/assets/fonts/Sono-Regular.ttf"),
   });
 
-  const [userNumber, setUserNumber] = useState();
-
-  const handleStartGame = (selectedNumber) => {
-    setUserNumber(selectedNumber);
-  };
-
-  let content = <StartGameScreen onStartGame={handleStartGame} />;
-
-  if (userNumber) {
-    content = <GameScreen />;
-  }
-
-  if (!loaded) {
+  if (!fontsLoaded) {
     return null;
   }
 
-  return (
-    <View style={styles.container}>
-      <Header title={"Adivina el numero"} newStyles={{ fontFamily: "Sono" }} />
-      {content}
-    </View>
-  );
+  return <ShopNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
--- constants/colors.js
+++
@@ -1,6 +0,0 @@
-export default {
-  primary: "#DDA15E",
-  secondary: "#BC6C25",
-  tertiary: "#FEFAE0",
-  white: "#edf6f9",
-};
